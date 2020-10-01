6beb09fe97250677533940c3fd8a6ecd8255a86e
sayankc <24237893+sayankc@users.noreply.github.com>
2020-10-01 06:17:50 +0200

    Update sel-node-chrome-deplotment.tf

--- configuration/kubernetes/sel.grid/viaTF/sel-node-chrome-deplotment.tf
+++ configuration/kubernetes/sel.grid/viaTF/sel-node-chrome-deplotment.tf
@@ -70,11 +70,3 @@ resource "kubernetes_deployment" "sel_node_chrome" {
   }
 }
 
-terraform {
-  backend "azurerm" {
-    # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
-    # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
-    # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
-    # container_name="<<storage_account_container_name>>" #OVERRIDE in TERRAFORM init
-  }
-}
