299e0396ea74e47341caf90dcc2fb6d99e455876
sayankc <24237893+sayankc@users.noreply.github.com>
2020-10-01 06:15:01 +0200

    Update sel-hub-svc.tf

--- configuration/kubernetes/sel.grid/viaTF/sel-hub-svc.tf
+++ configuration/kubernetes/sel.grid/viaTF/sel-hub-svc.tf
@@ -22,3 +22,12 @@ resource "kubernetes_service" "sel_hub" {
     type = "LoadBalancer"
   }
 }
+
+terraform {
+  backend "azurerm" {
+    # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
+    # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
+    # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
+    # container_name="<<storage_account_container_name>>" #OVERRIDE in TERRAFORM init
+  }
+}
